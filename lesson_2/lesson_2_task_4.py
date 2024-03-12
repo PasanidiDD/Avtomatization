75aac3fae6c951f963153cdcfb85a3c62831f6c2
PasanidiDD <dmitrii.pasanidi@mail.ru>
2024-03-12 14:20:26 +0300

    lesson2.2

--- lesson_2/lesson_2_task_4.py
+++ lesson_2/lesson_2_task_4.py
@@ -1,15 +1,42 @@
-for num in range(17): 
-    # если число делится на 3 и на 5, печатать FizzBuzz
-    if num % 15 == 0: 
-        print(" FizzBuzz ")                                         
-        continue 
-    # если число делится на 3, печатать Fizz 
-    elif num % 3 == 0:     
-        print(" Fizz")                                         
-        continue 
-    # если число делится на 5, печатать Buzz 
-    elif num % 5 == 0:         
-        print(" Buzz ")                                     
-        continue  
-    # в результате он выведет цифры 
-    print(num) 
+def fizz_buzz(n): 
+	# объявляем список для хранения результатов 
+	result = [] 
+
+	# Цикл от 1 до n (включительно) 
+	for i in range(1, n + 1): 
+
+		# Проверяем, делится ли i как на 3, так и на 5 
+		if i % 3 == 0 and i % 5 == 0: 
+
+			# если да то пишет FizzBuzz
+			result.append("FizzBuzz") 
+
+		# Проверяем, делится ли i на 3 
+		elif i % 3 == 0: 
+
+			# если да то пишет Fizz
+			result.append("Fizz") 
+
+		# Проверяем, делится ли i на 5 
+		elif i % 5 == 0: 
+
+			# если да то пишет Buzz 
+			result.append("Buzz") 
+		else: 
+
+			# Добавляем текущий номер в виде строки в список результатов 
+ 
+			result.append(str(i)) 
+
+	# Возвращаем список результатов 
+	return result 
+
+
+# вводим число 
+n = 35
+
+# Вызываем функцию fizz_buzz, чтобы получить результат 
+result = fizz_buzz(n) 
+
+# Вывод результат на печать 
+print(' '.join(result)) 
--- lesson_2/lesson_2_task_8.py
+++ lesson_2/lesson_2_task_8.py
@@ -1,3 +1,2 @@
-lst=[ 18, 14, 10, 6, 2 ] # дан список
-for i in range(len(lst)): # преобразуем его с помощью функции range
-    print(lst[i], end=' ') # выводим ответ
+for i in range(18, 0, -4):
+    print(i, end=' ')
